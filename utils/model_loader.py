277e740eceb7f3a344f1b3ee6aaf5f5ece2a127f
Mauricio Molina <mauricioleonelmolina@hotmail.com>
2026-06-01 15:13:19 -0300

    Add Streamlit caching to model loading function

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -1,10 +1,10 @@
 import joblib
 import os
+import streamlit as st
 
+@st.cache_resource
 def cargar_modelo():
-    
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    
     model_path = os.path.join(base_dir, "..", "modelo_svm_vozactiva.pkl")
     
     if not os.path.exists(model_path):
