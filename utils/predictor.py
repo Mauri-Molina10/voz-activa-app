deb0f39e7d517ba3d847184c813bb879efc6d844
Mauricio Molina <mauricioleonelmolina@hotmail.com>
2026-06-01 15:14:08 -0300

    Update predictor.py

--- utils/predictor.py
+++ utils/predictor.py
@@ -1,29 +1,15 @@
+import gc # Importante: recolector de basura
 from sentence_transformers import SentenceTransformer
-
 from utils.audio_processing import transcribir_audio
 from utils.model_loader import cargar_modelo
 
-# ======================================
-# CARGA GLOBAL
-# ======================================
-
+# Carga única
 modelo_svm = cargar_modelo()
-
-modelo_embeddings = SentenceTransformer(
-    "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-)
-
+modelo_embeddings = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
 clases = ["agua", "ayuda", "dolor"]
 
-
-# ======================================
-# PREDICCIÓN
-# ======================================
-
-
 def predecir_audio(audio_path):
-
-    # 1. Transcribir audio
+    # 1. Transcribir
     texto = transcribir_audio(audio_path)
 
     # 2. Embedding
@@ -31,14 +17,14 @@ def predecir_audio(audio_path):
 
     # 3. Predicción
     prediccion = modelo_svm.predict(embedding)[0]
-
-    # 4. Probabilidades
     probabilidades = modelo_svm.predict_proba(embedding)[0]
 
-    probs_dict = {
-        clases[i]: float(probabilidades[i])
-        for i in range(len(clases))
-    }
+    # --- LIBERACIÓN DE MEMORIA ---
+    del embedding # Borramos el objeto de memoria
+    gc.collect()  # Forzamos al sistema a limpiar RAM
+    # -----------------------------
+
+    probs_dict = {clases[i]: float(probabilidades[i]) for i in range(len(clases))}
 
     return {
         "texto": texto,
